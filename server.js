2a40f34944e666dafef4a107e7f71bd0a98b8e49
jazzcruz <jazellecruz22@gmail.com>
2023-07-06 16:38:41 +0800

    add middlewares & initialize db

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+require("dotenv").config();
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+const {connectToDB} = require("./src/db/db");
+const errorHandler = require("./src/middlewares/error");
+const main = require("./src/controllers/index");
 
-app.listen(2424, (req, res) => {
-  console.log("Huzzah! Successfully running on port 2424!");
+// configurations
+app.use(cors());
+app.use(express.json());
+
+// connect to database
+connectToDB();
+
+// main app routes
+app.use(main)
+
+// for error handling
+app.use(errorHandler);
+
+app.listen(8000, (req, res) => {
+  console.log("Huzzah! Successfully running on port 8000!");
 });
