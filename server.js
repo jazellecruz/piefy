b06fb6993f05af5411b3e0c9f6b49ab751909539
jazzcruz <jazellecruz22@gmail.com>
2023-07-20 21:50:41 +0800

    fix options for urlencoded middleware

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const client = require("./src/controllers/client");
 // configurations
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extenderd: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
