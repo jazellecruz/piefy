1729123a04ec94f102ba2e1a5eadf477435769ce
jazzcruz <jazellecruz22@gmail.com>
2023-08-22 14:35:48 +0800

    fixing the path of public  dir

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 // connect to database
 connectToDB();
