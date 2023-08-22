f6971bbc587f7aa32127de7ef3a40ade4907fa2c
jazzcruz <jazellecruz22@gmail.com>
2023-08-22 14:28:35 +0800

    fixing the path of views dir

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const {ignoreFiles} = require("./src/middlewares/common");
 const api = require("./src/controllers/api");
 const client = require("./src/controllers/client");
 const error = require("./src/controllers/error.js")
+const path = require("path");
 
 // configurations
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
